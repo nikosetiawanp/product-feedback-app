49ee8e363b8dbfe7abf5d64509c80501aff9a6f2
nikosetiawanp <nikosetiawanp@gmail.com>
2023-07-14 11:47:22 +0700

    updated redirect to login page 2

--- src/pages/RedirectToLogin.tsx
+++ src/pages/RedirectToLogin.tsx
@@ -1,7 +1,7 @@
 import SpinnerDark from "../assets/shared/spinner-dark.svg";
 
 export default function RedirectToLogin() {
-  //   window.location.href = "./login";
+  window.location.href = "./login";
   return (
     <div className="redirect-to-login-page">
       <img src={SpinnerDark} alt="spinner-dark" />
