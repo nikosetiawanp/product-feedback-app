5e47551f1ff232715c814552f5e86ae2f0478114
nikosetiawanp <nikosetiawanp@gmail.com>
2023-07-14 11:51:58 +0700

    removed spinner from redirect to login page

--- src/pages/RedirectToLogin.tsx
+++ src/pages/RedirectToLogin.tsx
@@ -1,10 +1,4 @@
-import SpinnerDark from "../assets/shared/spinner-dark.svg";
-
 export default function RedirectToLogin() {
   window.location.href = "./login";
-  return (
-    <div className="redirect-to-login-page">
-      <img src={SpinnerDark} alt="spinner-dark" />
-    </div>
-  );
+  return <div></div>;
 }
